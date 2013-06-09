a885b96cd60b8e7ae19b39391b9b9d128f8956f4
Joe Stringer <joestringernz@gmail.com>
2013-06-09 12:43:37 +1200

    build: Add missing ryu-rfproxy dependencies
    Signed-off-by: Joe Stringer <joestringernz@gmail.com>

--- dist/build_ryu.sh
+++ dist/build_ryu.sh
@@ -3,6 +3,8 @@
 RYU_GIT="https://github.com/joestringer/ryu-rfproxy.git"
 RYU_BRANCH="origin/master"
 
+RYU_DEPS="python-gevent python-webob python-routes"
+
 get_ryu() {
     # TODO: This recommendation should be revisited after the ovs-1.10+ release
     if [ "$OVS_VERSION" != "git" ]; then
@@ -10,10 +12,11 @@ get_ryu() {
             $YELLOW
     fi
 
+    pkg_install "$MONGO_DEPS"
     print_status "Fetching Ryu controller"
 
     if [ $FETCH_ONLY -ne 1 ]; then
-        $SUPER pip install ryu || fail "Failed to fetch ryu controller"
+        $SUPER pip install oslo.config ryu || fail "Failed to fetch ryu controller"
     fi
 
     fetch "ryu-" "rfproxy" $RYU_GIT $RYU_BRANCH ||
