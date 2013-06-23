94031d70b6370bf667b1f377c40f527b3b018b6a
Joe Stringer <joestringernz@gmail.com>
2013-06-23 22:34:21 +0900

    Lock Ryu platform to version 2.0
    Signed-off-by: Joe Stringer <joestringernz@gmail.com>

--- dist/build_ryu.sh
+++ dist/build_ryu.sh
@@ -4,6 +4,7 @@ RYU_GIT="https://github.com/routeflow/ryu-rfproxy.git"
 RYU_BRANCH="origin/master"
 
 RYU_DEPS="python-greenlet python-eventlet python-webob python-routes"
+RYU_VERSION="2.0"
 
 get_ryu() {
     if [ "$OVS_VERSION" != "git" ]; then
@@ -15,7 +16,7 @@ get_ryu() {
     print_status "Fetching Ryu controller"
 
     if [ $FETCH_ONLY -ne 1 ]; then
-        $SUPER pip install oslo.config ryu ||
+        $SUPER pip install oslo.config ryu==$RYU_VERSION ||
             fail "Failed to fetch ryu controller"
     fi
 
