95653432255aedca3c8b8c5e7c9c1f2b9a537b33
Joe Stringer <joe@wand.net.nz>
2013-07-26 14:36:44 +0900

    Don't keep track of configured RFVS instances
    We have previously been keeping track of RFVS instances that have been
    configured, but this was not used anywhere. Furthermore, when RFVS
    instances disconnected from RouteFlow, they would still be considered
    "configured" even if they have since lost all of their state.
    
    This patch removes the RFServer.configured_rfvs[] list. This makes
    RouteFlow more resilient to temporary RFVS outages, and simplifies the
    code.
    
    Signed-off-by: Joe Stringer <joe@wand.net.nz>

--- rfserver/rfserver.py
+++ rfserver/rfserver.py
@@ -32,7 +32,6 @@ class RFServer(RFProtocolFactory, IPC.IPCMessageProcessor):
         self.isltable = RFISLTable()
         self.config = RFConfig(configfile)
         self.islconf = RFISLConf(islconffile)
-        self.configured_rfvs = []
         # Logging
         self.log = logging.getLogger("rfserver")
         self.log.setLevel(logging.INFO)
@@ -335,10 +334,8 @@ class RFServer(RFProtocolFactory, IPC.IPCMessageProcessor):
         self.ipc.send(RFSERVER_RFPROXY_CHANNEL, str(ct_id), rm)
 
     def config_dp(self, ct_id, dp_id):
-        if is_rfvs(dp_id) and dp_id not in self.configured_rfvs:
-            # If rfvs is coming up and we haven't configured it yet, do it
+        if is_rfvs(dp_id):
             # TODO: support more than one OVS
-            self.configured_rfvs.append(dp_id)
             self.send_datapath_config_message(ct_id, dp_id, DC_ALL)
             self.log.info("Configuring RFVS (dp_id=%s)" % format_id(dp_id))
         elif self.rftable.is_dp_registered(ct_id, dp_id) or \
