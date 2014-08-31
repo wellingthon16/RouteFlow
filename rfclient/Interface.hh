678a9aff16f2979113ba3d7cd4b0dea6021a2db9
Josh Bailey <joshb@google.com>
2014-08-31 20:47:49 +1200

    * Physical/VLAN attributes for interfaces.

--- rfclient/Interface.hh
+++ rfclient/Interface.hh
@@ -1,6 +1,7 @@
 #ifndef INTERFACE_HH
 #define INTERFACE_HH
 
+#include "converter.h"
 #include "types/IPAddress.h"
 #include "types/MACAddress.h"
 
@@ -11,9 +12,14 @@ class Interface {
         std::vector<IPAddress> addresses;
         MACAddress hwaddress;
         bool active;
+        bool physical;
+        uint32_t vlan;
 
         Interface() {
             this->active = false;
+            this->physical = false;
+            this->port = 0;
+            this->vlan = 0;
         }
 
         Interface& operator=(const Interface& other) {
@@ -23,17 +29,37 @@ class Interface {
                 this->addresses = other.addresses;
                 this->hwaddress = other.hwaddress;
                 this->active = other.active;
+                this->physical = other.physical;
+                this->vlan = other.vlan;
             }
             return *this;
         }
 
         bool operator==(const Interface& other) const {
             return
-                (this->port == other.port) and
-                (this->name == other.name) and
-                (this->addresses == other.addresses) and
-                (this->hwaddress == other.hwaddress) and
-                (this->active == other.active);
+                (this->port == other.port) &&
+                (this->name == other.name) &&
+                (this->addresses == other.addresses) && 
+                (this->hwaddress == other.hwaddress) &&
+                (this->active == other.active) &&
+                (this->physical == other.physical) &&
+                (this->vlan == other.vlan);
+        }
+        
+        string toString() {
+            string description = this->name;
+            description += " port: " + to_string<uint32_t>(this->port);
+            description += " physical: " + to_string<bool>(this->physical);
+            description += " active: " + to_string<bool>(this->active);
+            description += " vlan: " + to_string<uint32_t>(this->vlan);
+            description += " hwaddress: " + this->hwaddress.toString();
+            description += " addresses:";
+            for (std::vector<IPAddress>::iterator it = this->addresses.begin();
+                 it != this->addresses.end();
+                 ++it) {
+                description += " " + it->toString();
+            }
+            return description;
         }
 };
 
