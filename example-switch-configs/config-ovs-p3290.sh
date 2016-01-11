fc03089942d30a7bcb2f3d78927e1813123e6ff1
Josh Bailey <joshb@google.com>
2016-01-11 04:35:49 +0000

    Add example config for Pica8 3290.

---
+++ example-switch-configs/config-ovs-p3290.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# minimal configuration for Pica8 3290
+# known to work at Picos 2.6.4
+
+CTLIP=192.168.3.5
+DPID=0000000000000099
+BR=br0
+
+ovs-vsctl del-br $BR
+ovs-vsctl add-br $BR -- set bridge $BR datapath_type=pica8
+ovs-vsctl set bridge $BR datapath_type=pica8
+ovs-vsctl add-port $BR ge-1/1/1 -- set Interface ge-1/1/1 type=pica8
+ovs-vsctl add-port $BR ge-1/1/2 -- set Interface ge-1/1/2 type=pica8
+ovs-vsctl add-port $BR ge-1/1/48 -- set Interface ge-1/1/48 type=pica8
+ovs-vsctl set bridge $BR other-config:datapath-id=$DPID
+ovs-vsctl set-controller $BR tcp:$CTLIP:6653
