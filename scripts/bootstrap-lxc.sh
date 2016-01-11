30bb09dd9b541daa5c63f7e9818777e747dce723
Josh Bailey <joshb@google.com>
2016-01-11 05:06:00 +0000

    Needs to be mounted executable.

--- scripts/bootstrap-lxc.sh
+++ scripts/bootstrap-lxc.sh
@@ -7,5 +7,5 @@ modprobe zram
 umount /var/lib/lxc
 echo 2048M > /sys/block/zram0/disksize
 mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
-mount -o relatime,noexec,nosuid /dev/zram0 /var/lib/lxc
+mount -o relatime /dev/zram0 /var/lib/lxc
 tar zxvf /home/projectw/clean-rfvm1-b1-b2.tgz
