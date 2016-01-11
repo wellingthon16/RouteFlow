6e73e1d522241ca5ba3d4d2a90d57608f235add4
Josh Bailey <joshb@google.com>
2016-01-11 06:02:30 +0000

    Missing VLAN strip action.

--- rfserver/rffastpath.py
+++ rfserver/rffastpath.py
@@ -108,7 +108,7 @@ class MetaVLAN(MetaLabel):
         return action
 
     def rfaction_pop_meta(self, routemod):
-        routemod.add_action(Action.STRIP_VLAN())
+        routemod.add_action(Action.STRIP_VLAN_DEFERRED())
         return routemod
 
     def ofmatch_meta(self, parser, label, match=None):
